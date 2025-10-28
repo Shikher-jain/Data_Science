d7bafdc3754356cd92c2251617a0ff3ed6d89719
Shikher-jain <shikherjain786@gmail.com>
2025-10-29 02:44:16 +0530

    add datasets

---
+++ Steps/samples/sample.sql
@@ -0,0 +1,5 @@
+-- SQL sample: create table and insert 3 rows
+CREATE TABLE sample (A INTEGER, B TEXT, C REAL);
+INSERT INTO sample (A, B, C) VALUES (1, 'foo', 3.14);
+INSERT INTO sample (A, B, C) VALUES (2, 'bar', 2.718);
+INSERT INTO sample (A, B, C) VALUES (3, 'baz', 1.618);
